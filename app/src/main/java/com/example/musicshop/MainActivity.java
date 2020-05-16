97f80ec9ee2d4da30f9881b7efe7d9a965a59095
KaRDiNoVill <62694030+KaRDiNoVill@users.noreply.github.com>
2020-05-16 19:55:54 +0600

    added class Order plus checking with Logcat

--- app/src/main/java/com/example/musicshop/MainActivity.java
+++ app/src/main/java/com/example/musicshop/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.musicshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -23,10 +25,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     HashMap goodsMap;
     String goodsName;
     double price;
+    EditText userNameEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userNameEditText = findViewById(R.id.userNameEditText);
 
         createSpinner();
         createHashMap();
@@ -80,16 +85,23 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         ImageView goodsImageView = findViewById(R.id.goodsImageView);
         TextView informationTextView = findViewById(R.id.informationTextView);
+        TextView quantityTextView = findViewById(R.id.quantityTextView);
         switch (goodsName){
             case ("keyboard"):
+//                quantityTextView.setText("0");
+//                quantity = 0;
                 goodsImageView.setImageResource(R.drawable.keyboard);
                 informationTextView.setText("New Bösendorfer 225 Grand Piano\nThe 225, like the bigger brother 290 Imperial – has been crafted dedicating to the tradition of Viennese piano building.");
                 break;
             case ("drums"):
+//                quantityTextView.setText("0");
+//                quantity = 0;
                 goodsImageView.setImageResource(R.drawable.drums);
                 informationTextView.setText("Yamaha - Stage Custom Birch \n6-Pc Drum Set (10,12,14,16, Sn, 22) \nw/Hardware - Raven Black 100% Birch shell drums, 6-ply\n1.6mm steel hoops");
                 break;
             case ("music plate"):
+//                quantityTextView.setText("0");
+//                quantity = 0;
                 goodsImageView.setImageResource(R.drawable.music_plate);
                 informationTextView.setText("Bohemian Rhapsody \nPiano, Vocal and Guitar Piano-Vocal-Guitar Songbook. product type Book Only. A product from Hal Leonard");
                 break;
@@ -99,5 +111,18 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    public void addToCart(View view) {
+        Order order = new Order();
+
+        order.userName = userNameEditText.getText().toString();
+        Log.d("userName", order.userName); // for checking work it or not work in debug
+        order.goodsName = goodsName;
+        Log.d("goodsName", order.goodsName);
+        order.quantity = quantity;
+        Log.d("quantity", String.valueOf(order.quantity));
+        order.orderPrice = quantity * price;
+        Log.d("userName", String.valueOf(order.orderPrice));
     }
 }
